40b656f408930adae863dd9720788f8086b631df
Aritra Ghosh <aritrag94@gmail.com>
2021-03-02 17:25:25 -0500

    Added create and get order API

--- service/models.py
+++ service/models.py
@@ -1,9 +1,10 @@
 """
-Models for YourResourceModel
+Models for Order
 
 All of the models are stored in this module
 """
 import logging
+from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 
 logger = logging.getLogger("flask.app")
@@ -16,19 +17,25 @@ class DataValidationError(Exception):
     pass
 
 
-class YourResourceModel(db.Model):
+class Item(db.Model):
     """
-    Class that represents a <your resource model name>
+    Class that represents an item inside an order... For eg if a person orders 3 oranges and 4 apples as 
+    part of a single order, this Item class represents the 3 oranges. 
     """
 
     app = None
 
-    # Table Schema
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(63))
+    # Order Item Table Schema
+    item_id = db.Column(db.Integer, primary_key=True)
+    product_id = db.Column(db.Integer, nullable = False)
+    price = db.Column(db.Float, nullable=False)
+    quantity = db.Column(db.Integer, nullable=False, default=1)
+    # TODO:  Add status
+    # The order id has to be stored in another table as the different items have the same order id
+    order_id = db.Column(db.Integer, db.ForeignKey('order.id'))
 
     def __repr__(self):
-        return "<YourResourceModel %r id=[%s]>" % (self.name, self.id)
+        return "<Item %r product_id=[%r]>" % (self.item_id, self.product_id)
 
     def create(self):
         """
@@ -41,40 +48,144 @@ class YourResourceModel(db.Model):
 
     def save(self):
         """
-        Updates a YourResourceModel to the database
+        Updates an Item to the database
         """
         logger.info("Saving %s", self.name)
         db.session.commit()
 
     def delete(self):
-        """ Removes a YourResourceModel from the data store """
+        """ Removes an item from the data store """
         logger.info("Deleting %s", self.name)
         db.session.delete(self)
         db.session.commit()
 
     def serialize(self):
-        """ Serializes a YourResourceModel into a dictionary """
+        """ Serializes an item  into a dictionary """
         return {
-            "id": self.id,
-            "name": self.name
+            "item_id": self.item_id,
+            "product_id": self.product_id,
+            "quantity": self.quantity,
+            "price": self.price
         }
 
     def deserialize(self, data):
         """
-        Deserializes a YourResourceModel from a dictionary
+        Deserializes an Item  from a dictionary
 
         Args:
             data (dict): A dictionary containing the resource data
         """
         try:
-            self.name = data["name"]
+            self.product_id   = data["product_id"]
+            self.quantity =  data["quantity"]
+            self.price = data["price"]
+
+            if self.product_id is None or not isinstance(self.product_id, int):
+                raise DataValidationError("Invalid order: check product id")
+            if self.quantity is None or not isinstance(self.quantity, int):
+                raise DataValidationError("Invalid Item: check quantity")
+            if self.price is None or \
+                    (not isinstance(self.price, float) and not isinstance(self.price, int)):
+                raise DataValidationError("Invalid Item: check price")
+
         except KeyError as error:
-            raise DataValidationError("Invalid YourResourceModel: missing " + error.args[0])
+            raise DataValidationError("Invalid Item: missing " + error.args[0])
         except TypeError as error:
             raise DataValidationError(
-                "Invalid YourResourceModel: body of request contained" "bad or no data"
+                "Invalid Item: body of request contained" "bad or no data"
             )
         return self
+
+    
+class Order(db.Model):
+    """ Class that represents an Order """
+    logger = logging.getLogger(__name__)
+    app = None
+
+    ##################################################
+    # Order Table Schema
+    ##################################################
+    id = db.Column(db.Integer, primary_key=True)
+    customer_id = db.Column(db.Integer, nullable=False)
+    creation_date = db.Column(db.DateTime(), default=datetime.now)
+    # List of items in the order 
+    order_items = db.relationship('Item', backref='order', cascade="all, delete", lazy=True)
+
+    def __repr__(self):
+        return "<Order %r>" % self.id
+
+
+    def create(self):
+        """
+        Creates an order in the DB
+        """
+        if self.customer_id is None:
+            raise DataValidationError("Invalid Order : Customer Id  empty")
+
+        if len(self.order_items) == 0:
+            raise DataValidationError("Invalid Order : Order Items  empty")
+
+        db.session.add(self)
+        db.session.commit()
+
+
+    def update(self):
+        """
+        Updates an Order to the database
+        """
+        if not self.id or not isinstance(self.id, int):
+            raise DataValidationError("Update called with invalid id field")
+        if self.customer_id is None or not isinstance(self.customer_id, int):
+            raise DataValidationError("Customer Id is not valid")
+        if len(self.order_items) == 0:
+            raise DataValidationError("Order Items can't be empty")
+        db.session.commit()
+
+    def delete(self):
+        """
+        Removes an Order from the Database
+        """
+        db.session.delete(self)
+        db.session.commit()
+
+    def serialize(self):
+        """ Serializes an order into a dictionary """
+        return {
+            "id": self.id,
+            "customer_id": self.customer_id,
+            "creation_date": self.creation_date,
+            "order_items": [order_item.serialize() for order_item in self.order_items]
+        }
+
+    def deserialize(self, data: dict):
+        """
+        Deserializes an Order from a dictionary
+        :param data: a dictionary of attributes
+        :type data: dict
+        :return: a reference to self
+        :rtype: Order
+        """
+        try:
+            self.customer_id = data["customer_id"]
+            # check if customer_id is integer
+            if self.customer_id is None or not isinstance(self.customer_id, int):
+                raise DataValidationError("Invalid Order: Customer Id must be integer")
+
+            items = data["order_items"]
+            if items is None or len(items) == 0:
+                raise DataValidationError("Order Items can't be empty")
+            for data_item in items:
+                item = Item()
+                item.deserialize(data_item)
+                self.order_items.append(item)
+        except KeyError as error:
+            raise DataValidationError("Invalid order: missing " + error.args[0])
+        except TypeError as error:
+            raise DataValidationError(
+                "Invalid order: body of request contained bad or no data"
+            )
+        return self
+
 
     @classmethod
     def init_db(cls, app):
@@ -88,13 +199,13 @@ class YourResourceModel(db.Model):
 
     @classmethod
     def all(cls):
-        """ Returns all of the YourResourceModels in the database """
-        logger.info("Processing all YourResourceModels")
+        """ Returns all of the Orders in the database """
+        logger.info("Processing all Orders")
         return cls.query.all()
 
     @classmethod
     def find(cls, by_id):
-        """ Finds a YourResourceModel by it's ID """
+        """ Finds a Order by it's ID """
         logger.info("Processing lookup for id %s ...", by_id)
         return cls.query.get(by_id)
 
--- service/routes.py
+++ service/routes.py
@@ -13,7 +13,7 @@ from flask_api import status  # HTTP Status Codes
 # For this example we'll use SQLAlchemy, a popular ORM that supports a
 # variety of backends including SQLite, MySQL, and PostgreSQL
 from flask_sqlalchemy import SQLAlchemy
-from service.models import YourResourceModel, DataValidationError
+from service.models import Order, Item,  DataValidationError
 
 # Import Flask application
 from . import app
@@ -27,6 +27,56 @@ def index():
     return "Reminder: return some useful information in json format about the service here", status.HTTP_200_OK
 
 
+
+@app.route("/orders/<int:order_id>", methods=["GET"])
+def get_orders(order_id):
+    """
+    Retrieve a single order
+    This endpoint will return a order based on it's id
+    """
+    app.logger.info("Request for order with id: %s", order_id)
+    order = Order.find(order_id)
+    if not order:
+        raise NotFound("Order with id '{}' was not found.".format(order_id))
+    return make_response(jsonify(order.serialize()), status.HTTP_200_OK)
+
+
+@app.route("/orders", methods=["POST"])
+def  create_order():
+    """
+    Creates an order
+    """
+    app.logger.info("Request to create an order")
+    check_content_type("application/json")
+    order = Order();
+    try:
+        order.deserialize(request.get_json())
+    except DataValidationError as dataValidationError:
+        api.abort(status.HTTP_400_BAD_REQUEST, dataValidationError)
+
+    order.create();
+    message = order.serialize()
+    location_url = url_for("get_orders", order_id=order.id, _external=True)
+    app.logger.info('Created Order with id: {}'.format(order.id))
+    return make_response(
+        jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
+    )
+
+
+
+@app.route("/orders", methods = ["GET"])
+def list_orders():
+    """
+    Returns list of all orders
+    """
+    app.logger.info("Request for order list")
+    orders = Order.all()
+    results = [order.serialize() for order in orders]
+    app.logger.info("Returning %d orders", len(results))
+    return make_response(jsonify(results), status.HTTP_200_OK)
+
+
+
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
 ######################################################################
@@ -34,4 +84,11 @@ def index():
 def init_db():
     """ Initialies the SQLAlchemy app """
     global app
-    YourResourceModel.init_db(app)
+    Order.init_db(app)
+
+def check_content_type(content_type):
+    """ Checks that the media type is correct """
+    if request.headers["Content-Type"] == content_type:
+        return
+    app.logger.error("Invalid Content-Type: %s", request.headers["Content-Type"])
+    abort(415, "Content-Type must be {}".format(content_type))
